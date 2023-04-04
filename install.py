ba0903ae0053c0aba197e07343ebda06b9e35470
Bingsu <ks2515@naver.com>
2023-04-04 23:50:31 +0900

    change pycocotools install

--- install.py
+++ install.py
@@ -1,3 +1,6 @@
+import platform
+import sys
+
 import torch
 from packaging import version
 
@@ -8,6 +11,21 @@ mmcv_url = {
     "2.0.0+cu118": "https://download.openmmlab.com/mmcv/dist/cu118/torch2.0.0/index.html",
 }
 
+pycocotools = {
+    "Windows": {
+        (3, 8): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp38-cp38-win_amd64.whl",
+        (3, 9): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp39-cp39-win_amd64.whl",
+        (3, 10): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp310-cp310-win_amd64.whl",
+        (3, 11): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp311-cp311-win_amd64.whl",
+    },
+    "Linux": {
+        (3, 8): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        (3, 9): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        (3, 10): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+        (3, 11): "https://github.com/Bing-su/dddetailer/releases/download/pycocotools/pycocotools-2.0.6-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
+    },
+}
+
 
 def check_mmcv() -> bool:
     if not is_installed("mmcv"):
@@ -15,7 +33,7 @@ def check_mmcv() -> bool:
 
     import mmcv
 
-    return version.parse(mmcv.__version__) >= version.parse("2.0.0rc1")
+    return version.parse(mmcv.__version__) >= version.parse("2.0.0rc4")
 
 
 def check_mmdet() -> bool:
@@ -24,7 +42,28 @@ def check_mmdet() -> bool:
 
     import mmdet
 
-    return version.parse(mmdet.__version__) >= version.parse("3.0.0rc0")
+    return version.parse(mmdet.__version__) >= version.parse("3.0.0rc6")
+
+
+def install_pycocotools():
+    system = platform.system()
+    machine = platform.machine()
+
+    if system not in ["Windows", "Linux"] or machine not in ["AMD64", "x86_64"]:
+        print("Installing pycocotools from pypi...")
+        run("pip install pycocotools")
+        return
+
+    links = pycocotools[system]
+    version = sys.version_info[:2]
+    if version not in links:
+        print("Installing pycocotools from pypi...")
+        run("pip install pycocotools")
+        return
+
+    url = links[version]
+    print("Installing pycocotools...")
+    run(f"pip install {url}")
 
 
 def install():
@@ -37,8 +76,7 @@ def install():
             run("pip install mmcv==2.0.0rc4")
 
     if not is_installed("pycocotools"):
-        print("Installing pycocotools...")
-        run("pip install aiartchan-pycocotools")
+        install_pycocotools()
 
     if not check_mmdet():
         print("Installing mmdet...")
